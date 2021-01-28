14588b0bbbbe125f0cc32426c508b7bc324453c1
Maikel <maikelmatamorosz@gmail.com>
2021-01-28 15:17:43 -0600

    Developer to Maikel

---
+++ Desarrollo/WebApiSupport/WebApiSupport/Models/Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApiSupport.Models
+{
+    public partial class Service
+    {
+        public Service()
+        {
+            EmployeeServices = new HashSet<EmployeeService>();
+            Issues = new HashSet<Issue>();
+        }
+
+        public int ServiceId { get; set; }
+        public string Name { get; set; }
+        public bool? State { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+
+        public virtual ICollection<EmployeeService> EmployeeServices { get; set; }
+        public virtual ICollection<Issue> Issues { get; set; }
+    }
+}
--- Desarrollo/WebApiSupport/WebApiSupport/Startup.cs
+++ Desarrollo/WebApiSupport/WebApiSupport/Startup.cs
@@ -1,15 +1,17 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace IF4101SupportApp
+namespace WebApiSupport
 {
     public class Startup
     {
@@ -23,7 +25,10 @@ namespace IF4101SupportApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+             .AddNewtonsoftJson(options =>
+         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+        );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,14 +38,8 @@ namespace IF4101SupportApp
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -48,9 +47,7 @@ namespace IF4101SupportApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
