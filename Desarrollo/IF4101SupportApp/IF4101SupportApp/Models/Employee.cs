6449ae1974176f7cbd84550d4f87dd6eaa028012
Maikel <maikelmatamorosz@gmail.com>
2021-01-28 14:39:44 -0600

    Employee Form y Inicio del listar Employees

---
+++ Desarrollo/IF4101SupportApp/IF4101SupportApp/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IF4101SupportApp.Models
+{
+    public class Employee
+    {
+
+        public int Employee_ID { get; set; }
+        public string DNI { get; set; }
+        public string Employee_Name { get; set; }
+        public string First_Surname { get; set; }
+        public string Second_Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Employee_Type { get; set; }
+        public int Supervised { get; set; }
+
+        
+
+        
+        
+    }
+}
