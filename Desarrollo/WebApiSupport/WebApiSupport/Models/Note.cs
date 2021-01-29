cbe9182cf8dc17307ed8480d6ed4beb9c681f62b
ArturoCamp <artcambo20@hotmail.com>
2021-01-28 18:10:27 -0600

    WebApiSupport 3.0

---
+++ Desarrollo/WebApiSupport/WebApiSupport/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApiSupport.Models
+{
+    public partial class Note
+    {
+        public int NoteId { get; set; }
+        public string Description { get; set; }
+        public byte[] NoteTimestamp { get; set; }
+        public int? ReportNumber { get; set; }
+        public bool? State { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+
+        public virtual Issue ReportNumberNavigation { get; set; }
+    }
+}
