9093791a7ca5bf25680037d9ce64c664c0b255c2
ArturoCamp <artcambo20@hotmail.com>
2021-02-02 15:10:54 -0600

    WebApiSupport 3.3

--- Desarrollo/WebApiSupport/WebApiSupport/Controllers/EmployeesController.cs
+++ Desarrollo/WebApiSupport/WebApiSupport/Controllers/EmployeesController.cs
@@ -51,6 +51,21 @@ namespace WebApiSupport.Controllers
             }
             return result;
         }
+        [Route("[action]/{id}")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> GetSupportById(int id)
+        {
+            ObjectResult result;
+            try
+            {
+                result = Ok(from l in _context.Employees where l.Supervised == id select new {l.EmployeeId,l.EmployeeName,l.FirstSurname });
+            }
+            catch (Exception e)
+            {
+                result = Conflict(e.Message);
+            }
+            return result;
+        }
         // GET: api/Employees
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
