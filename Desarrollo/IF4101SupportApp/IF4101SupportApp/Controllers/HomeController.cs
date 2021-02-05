0cb0edc7fabadab531aa55e2077fb87e2f734d92
ArturoCamp <artcambo20@hotmail.com>
2021-02-04 22:49:40 -0600

    WebApiSupport 3.6

--- Desarrollo/IF4101SupportApp/IF4101SupportApp/Controllers/HomeController.cs
+++ Desarrollo/IF4101SupportApp/IF4101SupportApp/Controllers/HomeController.cs
@@ -53,6 +53,9 @@ namespace IF4101SupportApp.Controllers
                 HttpContext.Session.SetInt32("role", employee.EmployeeType);
                 HttpContext.Session.SetInt32("id", employee.EmployeeId);
                 result = Ok(1);
+
+                
+
             }
             else if (Response.StatusCode == System.Net.HttpStatusCode.NotFound) {
                 result = NotFound(null);
