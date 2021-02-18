9bfea8aa2dcea0bb04418b7f8198119682090c5c
Maikel <maikelmatamorosz@gmail.com>
2021-02-18 09:15:54 -0600

    Cambios en la estructura de la base de datos del cliente

--- Documentation/IF4101ProyectoFinalArMaYeClientDB.sql
+++ Documentation/IF4101ProyectoFinalArMaYeClientDB.sql
@@ -1,71 +1,75 @@
 --create database IF4101ProyectoFinalArMaYe
 use IF4101ProyectoFinalArMaYe
 
-CREATE TABLE [User](
-	[user_id] int identity(1,1) primary key,
+CREATE TABLE Client(
+	client_Id int identity(1,1) primary key,
 	[name] nvarchar(30) not null,
-	first_surname nvarchar(40) not null,
-	second_surname nvarchar(50) not null,
+	first_Surname nvarchar(40) not null,
+	second_Surname nvarchar(50) not null,
 	adress nvarchar(200),
 	phone nvarchar(8),
 	email nvarchar(40) unique not null,
 	[password] VARBINARY(8000),
 	[state] bit, 
-    creation_date DATETIME Default GETDATE() not null,
-    modify_date DATETIME,
-    created_by int ,
-	modified_by int
+    creation_Date DATETIME Default GETDATE() not null,
+    modify_Date DATETIME,
+    created_By int ,
+	modified_By int
 )
 
-CREATE TABLE [Service](
-    service_id int IDENTITY(1,1) PRIMARY KEY,
+
+CREATE TABLE ServiceT(
+    service_Id int IDENTITY(1,1) PRIMARY KEY,
     [name] VARCHAR(20),
 	[state] bit, 
-	creation_date DATETIME Default GETDATE(),
-    modify_date DATETIME,
-    created_by int ,
-	modified_by int
+	creation_Date DATETIME Default GETDATE(),
+    modify_Date DATETIME,
+    created_By int ,
+	modified_By int
 )
 
-CREATE TABLE user_services(
-	[user_id] int foreign key references [User]([user_id]),
-	service_id int foreign key references [Service](service_id),
+
+CREATE TABLE Client_Services(
+	[client_Id] int foreign key references Client(client_Id),
+	service_Id int foreign key references ServiceT(service_Id),
 	[state] bit, 
-	creation_date DATETIME Default GETDATE() not null,
-    modify_date DATETIME,
-    created_by int ,
-	modified_by int,
-	primary key([user_id],service_id),
+	creation_Date DATETIME Default GETDATE() not null,
+    modify_Date DATETIME,
+    created_By int ,
+	modified_By int,
+	primary key(client_Id,service_Id),
 )
+
 
 CREATE TABLE Issue(
-	report_number int identity(1,1) primary key,
+	report_Number int identity(1,1) primary key,
 	[description] nvarchar(300) not null,
-	register_timestamp datetime default GETDATE(),
+	register_Timestamp datetime default GETDATE(),
 	adress nvarchar(200),
-	contact_phone nvarchar(8),
-	contact_email nvarchar(40) unique not null,
+	contact_Phone nvarchar(8),
+	contact_Email nvarchar(40) unique not null,
 	[status] Char CHECK([status] in('I','A','P','R')),
-	supporter_assigned int,
-	[service_id] int foreign key references [service](service_id),
-	[user_id] int foreign key references [user]([user_id]),
+	supporter_Assigned int,
+	service_Id int foreign key references ServiceT(service_Id),
+	client_Id int foreign key references Client(client_Id),
 	[state] bit, 
-    creation_date DATETIME Default GETDATE() not null,
-    modify_date DATETIME,
-    created_by int ,
-	modified_by int
+    creation_Date DATETIME Default GETDATE() not null,
+    modify_Date DATETIME,
+    created_By int ,
+	modified_By int
 )
 
+
 CREATE TABLE Comment(
-    comment_id int IDENTITY(1,1) PRIMARY KEY,
+    comment_Id int IDENTITY(1,1) PRIMARY KEY,
     [description] nvarchar(200),
-    comment_timestamp DATETIME default GETDATE(),
-    report_number int FOREIGN KEY REFERENCES Issue(report_number),
+    comment_Timestamp DATETIME default GETDATE(),
+    report_Number int FOREIGN KEY REFERENCES Issue(report_Number),
 	[state] bit, 
-	creation_date DATETIME Default GETDATE() not null,
-    modify_date DATETIME,
-    created_by int ,
-	modified_by int
+	creation_Date DATETIME Default GETDATE() not null,
+    modify_Date DATETIME,
+    created_By int ,
+	modified_By int
 )
 
 
